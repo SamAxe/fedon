1cacafd53b37e6350cd84f34b9e197ef35a908e5
SamAxe <693896-SamAxe@users.noreply.gitlab.com>
2024-11-29 12:39:37 -0800

    Checkpoint

--- bin/clitest2.ml
+++ bin/clitest2.ml
@@ -85,10 +85,55 @@ type action =
 
 type item_id = string [@@deriving show]
 
+type fw_story_item = Yojson.Safe.t
+type story_item =
+  { id      : item_id   (* id and type are lifted from fw_item for convienence, but should be read only and not changed *)
+  ; type_   : string
+  ; fw_item : fw_story_item
+  }
+
+let id_from_fw_item (json_item : Yojson.Safe.t ) =
+(*   Printf.printf "keys: %s\n in: %s\n" (Yojson.Safe.Util.keys json_item |> String.concat ", ") (Yojson.Safe.pretty_to_string json_item); *)
+  let item = json_item |> member "id" in
+  match item with
+  | `Null -> None
+  | _     -> Some (item |> to_string)
+
+let type_from_fw_item (json_item : Yojson.Safe.t ) =
+  let item = json_item |> member "type" in
+  match item with
+  | `Null -> None
+  | _     -> Some (item |> to_string)
+
+let withDefault ( default : string ) ( opt : string option ) : string =
+  match opt with
+  | None -> default
+  | Some x -> x
+
+
+let generateItemId () : string =
+  let x1 = Random.full_int Int.max_int in
+    Printf.sprintf "%016x" x1
+
+
+let fw_story_item_to_story_item (fw_story_item:fw_story_item) : story_item =
+  { id      = id_from_fw_item fw_story_item |> withDefault (generateItemId ())
+  ; type_   = type_from_fw_item fw_story_item |> withDefault "no type found"
+  ; fw_item = fw_story_item
+  }
+
+let _story_item_to_fw_story_item (story_item:story_item) : fw_story_item =
+  story_item.fw_item
+
+(* Journals are made of story_items because we want to be able to use
+   Ocaml's standard library to manipulate journal and story.  Atleast
+   for now.  A fw_story_item might make sense after I understand the
+   problem a bit better, but for now, conversions and wrapping...
+*)
 type journal_add =
   { after_id     : item_id
-  ; id           : item_id
-  ; item         : Yojson.Safe.t
+  ; item         : story_item
+
 (*     (error) *)
 (*
   ; fork
@@ -100,7 +145,7 @@ type journal_add =
   }
 
 type journal_create =
-  { item        : Yojson.Safe.t
+  { item        : story_item
 (*     (error) *)
 
 (*
@@ -112,8 +157,7 @@ type journal_create =
   }
 
 type journal_edit =
-  { item         : Yojson.Safe.t
-  ; id          : item_id
+  { item         : story_item
 (*     (error) *)
 (*
   ; fork
@@ -122,21 +166,21 @@ type journal_edit =
   }
 
 type journal_fork =
-  { item         : Yojson.Safe.t
+  { item         : story_item
 (*     (error) *)
 (*   ; site *)
   }
 
 type journal_move =
-  { from_id      : item_id
-  ; after_id     : item_id
+  { item      : story_item
+  ; after_id  : item_id
 (*     (error) *)
 (*   ; fork *)
 (*   ; id *)
   }
 
 type journal_remove =
-  { id           : item_id
+  { item      : story_item
 (*     (error) *)
 (*   ; removedTo *)
   }
@@ -156,31 +200,27 @@ type story_transaction =
 
 type journal = story_transaction list
 
-type story_item =
-  { id   : item_id
-  ; item : Yojson.Safe.t
-  }
 
 let pp_story_item (fmt:Format.formatter) (story_item:story_item) : unit =
-  let str = Printf.sprintf "{id=%s; item=%s}" (show_item_id story_item.id) (Yojson.Safe.to_string story_item.item) in
+  let str = Printf.sprintf "{id=%s; type=%s; item=%s}" (show_item_id story_item.id) story_item.type_ (Yojson.Safe.to_string story_item.fw_item) in
     Format.pp_print_string fmt str
 
-(*
 let show_story_item (story_item:story_item) : string =
-  Printf.sprintf "{id=%s; %s}" (show_item_id story_item.id) (Yojson.Safe.to_string story_item.item)
-*)
+  Printf.sprintf "{id=%s; %s}" (show_item_id story_item.id) (Yojson.Safe.to_string story_item.fw_item)
 
 type story = story_item list [@@deriving show]
 let empty_story = []
 
+(*
 type page =
   { _title : string
   ; _story : story
   }
+*)
 
-let _empty_page = { _title = ""; _story = [] }
+(* let _empty_page = { _title = ""; _story = [] } *)
 
-let add_id_to_item (add_item_id : item_id) (add_item : Yojson.Safe.t) : Yojson.Safe.t =
+let _add_id_to_item (add_item_id : item_id) (add_item : Yojson.Safe.t) : Yojson.Safe.t =
 
   let item_list =
     add_item
@@ -189,41 +229,30 @@ let add_id_to_item (add_item_id : item_id) (add_item : Yojson.Safe.t) : Yojson.S
   in
     `Assoc ( ("id", `String add_item_id) :: item_list )
 
-
-let rec apply_add_transaction_to_story_helper (add_item_id : item_id) (add_item : Yojson.Safe.t) (after_id : item_id) epilog prolog : story =
+let rec apply_add_transaction_to_story_helper (add_item : story_item) (after_id : item_id) (epilog:story) (prolog:story) : story =
   match prolog with
-  | []     -> { id = add_item_id; item = add_item} :: epilog
-  | [hd]   -> { id = add_item_id; item = add_item} :: hd :: epilog
+  | []     -> add_item :: epilog
+  | [hd]   -> add_item :: hd :: epilog
   | hd::tl ->
       begin
         if hd.id = after_id
-        then { id = add_item_id; item = add_item} :: hd :: tl @ epilog
-        else apply_add_transaction_to_story_helper add_item_id add_item after_id (hd :: epilog) tl
+        then add_item :: hd :: tl @ epilog
+        else apply_add_transaction_to_story_helper add_item after_id (hd :: epilog) tl
       end
 
 (* if after_id is not found, then appends at end of story *)
 let apply_add_transaction_to_story (journal_add:journal_add) (story:story) : story =
-
-  let item' = add_id_to_item journal_add.id journal_add.item in
-    story
-    |> apply_add_transaction_to_story_helper journal_add.id item' journal_add.after_id []
+  story
+  |> apply_add_transaction_to_story_helper journal_add.item journal_add.after_id []
 
 (* What does it mean to create a story that isn't empty?
 *)
-
-let itemId () : string =
-  let x1 = Random.full_int Int.max_int in
-(*
-  let x2 = Random.full_int Int.max_int in
-    Printf.sprintf "%08x%08x" x1 x2
-*)
-    Printf.sprintf "%08x" x1
 
 let apply_create_transaction_to_story (journal_create:journal_create) (story:story) : story =
 
   let helper prolog epilog : story =
     match prolog with
-    | []     -> { id = itemId (); item = journal_create.item} :: epilog
+    | []     -> journal_create.item :: epilog
     | _  -> failwith "Creating a story that isn't empty"
   in
     helper story []
@@ -232,11 +261,11 @@ let apply_edit_transaction_to_story (journal_edit:journal_edit) (story:story) : 
 
   let rec helper prolog epilog : story =
     match prolog with
-    | [] -> failwith (Printf.sprintf "Edit transaction didn't find valid id '%s'" journal_edit.id)
+    | [] -> failwith (Printf.sprintf "Edit transaction didn't find valid id '%s'" (show_story_item journal_edit.item) )
     | hd::tl ->
         begin
-          if hd.id = journal_edit.id
-          then (epilog |> List.rev) @ ( { id = ""; item = journal_edit.item } :: tl )
+          if hd.id = journal_edit.item.id
+          then (epilog |> List.rev) @ ( journal_edit.item :: tl )
           else helper tl (hd :: epilog)
         end
   in
@@ -244,25 +273,21 @@ let apply_edit_transaction_to_story (journal_edit:journal_edit) (story:story) : 
 
 (* Fork replaces the story with the forked story *)
 let apply_fork_transaction_to_story (journal_fork:journal_fork) (_story:story) : story =
-  let item_id = member "id" journal_fork.item |> to_string in
-  [ { id   = item_id ; item = journal_fork.item } ]
-
+  [ journal_fork.item ]
 
 let apply_remove_transaction_to_story_helper (remove_item_id:item_id) (story:story) : story =
   story
     |> List.filter ( function story_item -> story_item.id <>  remove_item_id )
 
 let apply_remove_transaction_to_story (journal_remove:journal_remove) (story:story) : story =
-  apply_remove_transaction_to_story_helper journal_remove.id story
+  apply_remove_transaction_to_story_helper journal_remove.item.id story
 
 (* Move is the same as remove + add, so we'll reuse those helper functions *)
 let apply_move_transaction_to_story (journal_move:journal_move) (story:story) : story =
-  let story_item = story |> List.find ( function story_item -> story_item.id = journal_move.from_id ) in
+  let story_item = story |> List.find ( function story_item -> story_item.id = journal_move.item.id ) in
   story
-    |> apply_remove_transaction_to_story_helper journal_move.from_id
-    |> apply_add_transaction_to_story_helper story_item.id story_item.item journal_move.after_id []
-
-
+    |> apply_remove_transaction_to_story_helper journal_move.item.id
+    |> apply_add_transaction_to_story_helper story_item journal_move.after_id []
 
 let apply_transaction_to_story (story:story) (story_transaction:story_transaction) : story =
   match story_transaction.action with
@@ -282,43 +307,44 @@ let story_from_journal (journal : journal ) : story =
 
 let _j1 =
   [ { date   = 124
-    ; action = Create { item = Yojson.Safe.from_string "{}" }
+    ; action = Create { item = Yojson.Safe.from_string "{}" |> fw_story_item_to_story_item }
     }
   ; { date   = 124
-    ; action = Add { id = "124"; after_id = "123"; item = Yojson.Safe.from_string "{}" }
+    ; action = Add { after_id = "123"; item = Yojson.Safe.from_string "{}" |> fw_story_item_to_story_item }
     }
   ; { date   = 124
-    ; action = Edit { id = "123"; item = Yojson.Safe.from_string "{}" }
+    ; action = Edit { item = Yojson.Safe.from_string "{}" |> fw_story_item_to_story_item }
     }
   ; { date   = 124
-    ; action = Fork { item = Yojson.Safe.from_string "{}" }
+    ; action = Fork { item = Yojson.Safe.from_string "{}" |> fw_story_item_to_story_item }
     }
   ; { date   = 124
-    ; action = Move { from_id = "123"; after_id = "246"; }
+    ; action = Move { item = Yojson.Safe.from_string "{}" |> fw_story_item_to_story_item; after_id = "246" }
     }
   ; { date   = 124
-    ; action = Remove { id = "123" }
+    ; action = Remove { item = Yojson.Safe.from_string "{}" |> fw_story_item_to_story_item }
     }
    ]
 
 let () =
+  Random.self_init ();
 
   let journal =
     [ { date   = 124
-      ; action = Create { item = Yojson.Safe.from_string {|{ "title": "A new page"}|} }
+      ; action = Create { item = Yojson.Safe.from_string {|{ "type": "create_page", "title": "A new page"}|} |> fw_story_item_to_story_item }
       }
     ; { date   = 125
-      ; action = Add { id = "125"; after_id = "124"; item = Yojson.Safe.from_string {|{ "type": "paragraph", "text":"Welcome to my new page"}|} }
+      ; action = Add { after_id = "124"; item = Yojson.Safe.from_string {|{ "id":"125", "type": "paragraph", "text":"Welcome to my new page"}|} |> fw_story_item_to_story_item }
       }
     ; { date   = 126
-      ; action = Add { id = "126"; after_id = "125"; item = Yojson.Safe.from_string {|{ "type": "paragraph", "text":"The middle paragraph"}|} }
+      ; action = Add { after_id = "125"; item = Yojson.Safe.from_string {|{ "id":"126", "type": "paragraph", "text":"The middle paragraph"}|} |> fw_story_item_to_story_item }
       }
 
     ; { date   = 127
-      ; action = Add { id = "127"; after_id = "126"; item = Yojson.Safe.from_string {|{ "type": "paragraph", "text":"The second middle paragraph"}|} }
+      ; action = Add { after_id = "126"; item = Yojson.Safe.from_string {|{ "id":"127", "type": "paragraph", "text":"The second middle paragraph"}|} |> fw_story_item_to_story_item }
       }
     ; { date   = 128
-      ; action = Add { id = "128"; after_id = "127"; item = Yojson.Safe.from_string {|{ "type": "paragraph", "text":"The third middle paragraph"}|} }
+      ; action = Add { after_id = "127"; item = Yojson.Safe.from_string {|{ "id":"128", "type": "paragraph", "text":"The third middle paragraph"}|} |> fw_story_item_to_story_item }
       }
      ]
   in
